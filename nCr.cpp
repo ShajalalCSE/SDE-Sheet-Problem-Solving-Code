47729177e8b829e03955d1267142a6cb59cf84c4
Sha jalal <shajalal.cse.cu@gmail.com>
2023-10-04 22:31:47 +0600

    nCr finding

--- nCr.cpp
+++ nCr.cpp
@@ -2,29 +2,42 @@
 using namespace std;
 typedef long long ll;
 
-ll nCr(int n,int r){
+ll nCr(int n, int r){
 
-
-    ll nominator=1,denomanator =1;
-
+    int res=1;
     for(int i=0;i<r;i++){
-        nominator*=(n-i);
-        denomanator*=(i+1);
-
+        res*=(n-i);
+        res=res/(i+1);
     }
-
-    ll res= nominator/denomanator;
 
     return res;
 
 }
+
 
 int main(){
 
     int n,r;
     cout<<"Enter n and r: ";
     cin>>n>>r;
-    long long ans=nCr(n,r);
+    ll ans=nCr(n,r);
     cout<<ans<<endl;
 
 }
+
+
+// ll nCr(int n,int r){
+
+
+    // ll nominator=1,denomanator =1;
+    // for(int i=0;i<r;i++){
+    //     nominator*=(n-i);
+    //     denomanator*=(i+1);
+
+    // }
+
+    // ll res= nominator/denomanator;
+
+    // return res;
+
+//}
